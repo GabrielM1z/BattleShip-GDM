64341372409487249b8017037aa393a8bc47f4ab
GabrielM1z <gabazoo.martinez@gmail.com>
2024-09-26 15:57:57 +0200

    model grid

--- BattleShip.Models/Grid.cs
+++ BattleShip.Models/Grid.cs
@@ -2,11 +2,11 @@ namespace BattleShip.Models
 {
     public class Grid
     {
-        public int[,] Matrix { get; set; }
+        public char[,] Matrix { get; set; }
 
         public Grid(int size)
         {
-            Matrix = new int[size, size];
+            Matrix = new char[size, size];
         }
     }
 }
