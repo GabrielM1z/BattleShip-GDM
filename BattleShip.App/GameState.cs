1ced9ccb4c2f193aba5e9249ba20a66b6acaa4b2
GabrielM1z <gabazoo.martinez@gmail.com>
2024-09-27 16:49:17 +0200

    front start fonctionnel

--- BattleShip.App/GameState.cs
+++ BattleShip.App/GameState.cs
@@ -1,15 +1,29 @@
 public class GameState
 {
     // Grille du joueur (lettres pour les bateaux, ' ' pour vide)
-    public char[,] PlayerGrid { get; set; }
+    public char[][] GridJ1 { get; set; }
 
     // Grille de l'adversaire (true = touché, false = raté, null = jamais tiré)
-    public bool?[,] OpponentGrid { get; set; }
+    public bool?[][] MaskedGridJ2 { get; set; }
 
     // Initialisation des grilles
     public GameState()
     {
-        PlayerGrid = new char[10, 10]; // Grille du joueur (10x10)
-        OpponentGrid = new bool?[10, 10]; // Grille de l'adversaire (10x10)
+        // Initialiser les grilles avec des tableaux de 10x10
+        GridJ1 = new char[10][];
+        MaskedGridJ2 = new bool?[10][];
+        
+        for (int i = 0; i < 10; i++)
+        {
+            GridJ1[i] = new char[10];
+            MaskedGridJ2[i] = new bool?[10];
+            
+            // Remplir la grille du joueur avec des espaces vides ' '
+            for (int j = 0; j < 10; j++)
+            {
+                GridJ1[i][j] = '\0'; // ou ' ' si tu veux un espace vide
+                MaskedGridJ2[i][j] = null; // Pas encore de tir sur cette grille
+            }
+        }
     }
 }
--- BattleShip.Models/Game.cs
+++ BattleShip.Models/Game.cs
@@ -1,8 +1,12 @@
-public class Game
+
+namespace BattleShip.Models
 {
-    public Guid Id { get; set; } 
-    public char[][] GridJ1 { get; set; }
-    public char[][] GridJ2 { get; set; }
-    public bool?[][] MaskedGridJ1 { get; set; }
-    public bool?[][] MaskedGridJ2 { get; set; }
+    public class Game
+    {
+        public Guid Id { get; set; } 
+        public char[][] GridJ1 { get; set; }
+        public char[][] GridJ2 { get; set; }
+        public bool?[][] MaskedGridJ1 { get; set; }
+        public bool?[][] MaskedGridJ2 { get; set; }
+    }
 }
