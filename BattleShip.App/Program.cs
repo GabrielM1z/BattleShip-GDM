b16a77071e67082d21c09bb2598ed964146e6da2
GabrielM1z <gabazoo.martinez@gmail.com>
2024-09-27 11:58:13 +0200

    oui

--- BattleShip.App/Program.cs
+++ BattleShip.App/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BattleShip.App;
 
-var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
