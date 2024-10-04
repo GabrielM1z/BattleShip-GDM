dec2c8ce63b8313ea33e59a33ded9ff7b51394df
IUT-Toulon-Maxence-Jamme <78029916+IUT-Toulon-Maxence-Jamme@users.noreply.github.com>
2024-10-04 10:55:07 +0200

    push model boat

--- BattleShip.Models/Boat.cs
+++ BattleShip.Models/Boat.cs
@@ -2,16 +2,20 @@ using System.Diagnostics;
 
 public class Boat
 {
+    public int Id { get; set;}
     public int Size { get; }
     public char Symbol { get; }
     public (int X, int Y) Coordinates { get; set; } // Propriété pour les coordonnées
     public Boolean Horizontal { get; set; }
+    public Boolean IsAlive { get; set; }
 
-    public Boat(int size, char symbol)
+    public Boat(int id, int size, char symbol)
     {
+        Id = id;
         Size = size;
         Symbol = symbol;
         Coordinates = (-1, -1); // Valeurs par défaut indiquant que les coordonnées sont inconnues
         Horizontal = true;
+        IsAlive = true;
     }
 }
--- BattleShip.Models/Fleet.cs
+++ BattleShip.Models/Fleet.cs
@@ -5,10 +5,28 @@ public class Fleet
     public Fleet()
     {
         Boats = new List<Boat>();
+        InitializeBoats(); // Appelle la méthode pour initialiser les bateaux
+    }
+
+    // Méthode pour ajouter des bateaux à la flotte
+    private void InitializeBoats()
+    {
+        AddBoat(new Boat(1, 2, 'A'));
+        AddBoat(new Boat(2, 3, 'B'));
+        AddBoat(new Boat(3, 3, 'C'));
+        AddBoat(new Boat(4, 4, 'D'));
+        AddBoat(new Boat(5, 5, 'E'));
+        
     }
 
     public void AddBoat(Boat boat)
     {
         Boats.Add(boat);
+    }
+
+    // Méthode pour obtenir la liste des bateaux
+    public List<Boat> GetBoats()
+    {
+        return Boats; // Retourne la liste des bateaux
     }
 }
